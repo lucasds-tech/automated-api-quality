c41885506ae00c97e14e2923cf3d5f4f277f896f
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-07-30 10:07:08 -0300

    initial commit project

---
+++ src/main/java/com/lstech/api_test_automation/ApiTestAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.lstech.api_test_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lstech/api_test_automation/ApiTestAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lstech.api_test_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
