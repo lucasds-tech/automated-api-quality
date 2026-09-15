27505dcb06c01c243872bacbfb4d21dd1eac4884
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-09-15 19:15:40 -0300

    feat: add CustomTestNGCucumberTestRest and CustomTestNGCucumberTestsWeb classes

---
+++ src/main/java/suport/CustomTestNGCucumberTestRest.java
@@ -0,0 +1,9 @@
+package suport;
+
+import lombok.extern.log4j.Log4j2;
+//import org.testng.annotations.AfterMethod;
+
+@Log4j2
+public class CustomTestNGCucumberTestRest extends CustomTestNGCucumberTestsWeb {
+
+}
---
+++ src/main/java/suport/CustomTestNGCucumberTestsWeb.java
@@ -0,0 +1,4 @@
+package suport;
+
+public class CustomTestNGCucumberTestsWeb {
+}
