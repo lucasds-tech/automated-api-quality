9c8f116793c84aee54f2659866e8625bd51ff683
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-09-15 18:58:11 -0300

    fix: update RestAssured setup initialization in Hooks

--- src/main/java/suport/hooks/Hooks.java
+++ src/main/java/suport/hooks/Hooks.java
@@ -1,10 +1,11 @@
 package suport.hooks;
 
+import com.lstech.api_test_automation.api.utils.RestAssuredConfig;
 import io.cucumber.java.Before;
 
 public class Hooks {
     @Before
     public void setUp() {
-        RestAssuredSetup.init();
+        RestAssuredConfig.init();
     }
 }
