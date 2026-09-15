735cab8540b53c5688099a0f50e8e998fd81cd5f
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-09-15 19:11:07 -0300

    feat: add ResidentRegistrationSteps for Cucumber tests

---
+++ src/main/java/com/lstech/api_test_automation/api/safeentry/steps/ResidentRegistrationSteps.java
@@ -0,0 +1,28 @@
+package com.lstech.api_test_automation.api.safeentry.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ResidentRegistrationSteps {
+
+    // Cenário 1: Cadastrar Um Morador Com Sucesso
+    @Given("que o nome, sobrenome, documento, residencia sao validos")
+    public void que_o_nome_sobrenome_documento_residencia_sao_validos() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("o sistema enviar uma requisicao POST com dados validos para API SafeEntry")
+    public void o_sistema_enviar_uma_requisicao_post_com_dados_validos_para_api_safe_entry() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("retorna status code {int} e uma mensagem de sucesso")
+    public void retorna_status_code_e_uma_mensagem_de_sucesso(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
