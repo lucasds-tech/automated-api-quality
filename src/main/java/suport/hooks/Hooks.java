302e92219f18c3f130130fe6d7e7692f8055e957
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-09-17 23:09:46 -0300

    feat: update import path for RestAssuredConfig in Hooks class

--- src/main/java/suport/hooks/Hooks.java
+++ src/main/java/suport/hooks/Hooks.java
@@ -1,11 +1,13 @@
 package suport.hooks;
 
-import com.lstech.api_test_automation.api.utils.RestAssuredConfig;
+import com.lstech.api_test_automation.api.common.util.RestAssuredConfig;
 import io.cucumber.java.Before;
 
 public class Hooks {
+
     @Before
     public void setUp() {
         RestAssuredConfig.init();
     }
+
 }
