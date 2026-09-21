3c5d458abe25558a10907079e34235b7de145565
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-09-20 21:32:01 -0300

    feat: implement DataGenerator for resident registration data generation

--- src/main/java/com/lstech/api_test_automation/api/common/generator/DataGenerator.java
+++ src/main/java/com/lstech/api_test_automation/api/common/generator/DataGenerator.java
@@ -1,4 +1,41 @@
 package com.lstech.api_test_automation.api.common.generator;
 
+import com.lstech.api_test_automation.api.config.EnvironmentConfig;
+import com.lstech.api_test_automation.api.safeentry.model.request.RequestResidentRegistration;
+import lombok.Builder;
+import lombok.Data;
+
 public class DataGenerator {
+
+    private final EnvironmentConfig environmentConfig;
+
+    public DataGenerator(EnvironmentConfig environmentConfig) {
+        this.environmentConfig = environmentConfig;
+    }
+
+    // ##### SAFEENTRY RESIDENT REGISTRATION ###################################################################
+    public ResidentRegistrationData generateResidentData() {
+        String nome = environmentConfig.getNome();
+        String sobrenome = environmentConfig.getSobrenome();
+        String documento = environmentConfig.getDocumento();
+        String residencia = environmentConfig.getResidencia();
+
+        RequestResidentRegistration requestResidentRegistration = RequestResidentRegistration.builder()
+                .nome(nome)
+                .sobrenome(sobrenome)
+                .documento(documento)
+                .residencia(residencia)
+                .build();
+
+        return ResidentRegistrationData.builder()
+                .requestResidentRegistration(requestResidentRegistration)
+                .build();
+    }
+
+    @Data
+    @Builder
+    public static class ResidentRegistrationData {
+        private RequestResidentRegistration requestResidentRegistration;
+    }
+
 }
